06113003c6130b92a9b9062aa6baddd1fc0725a9
Greg <surfingreg@swimr.com>
2025-02-13 15:49:51 -0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+1. https://docs.esp-rs.org/book/writing-your-own-application/generate-project/index.html
+
+2. Add partitions.csv and espflash.toml
+
+3. cargo run
---
+++ flash.sh
@@ -0,0 +1,4 @@
+
+. /home/gp/export-esp.sh
+# espflash flash --monitor --partition-table=part_4mb.csv target/xtensa-esp32s3-espidf/release/esp-ota-std-ex
+cargo run 
